381fd420eebaef1eab77a17dcff04ac32d09d916
Joni Turunen <jttturunen@gmail.com>
2016-12-18 21:37:38 +0200

    step-2, creating & updating

--- src/index.js
+++ src/index.js
@@ -2,7 +2,62 @@ function dom(type, props, ...children) {
     return {type, props, children};
 }
 
+function createElement(node) {
+    if (typeof node === 'string') {
+        return document.createTextNode(node);
+    } else {
+        const $el = document.createElement(node.type);
+        setProps($el, node.props);
 
+        node.children
+            .map(createElement)
+            .forEach($el.appendChild.bind($el));
+        return $el;
+    }
+}
+
+function setProps($target, props) {
+    if (!props) {
+        return;
+    }
+    Object.keys(props).forEach(key => {
+        setProp($target, key, props);
+    });
+}
+
+function setProp($target, key, props) {
+    $target[key] = props[key];
+}
+
+function updateElement($parent, newNode, oldNode, index = 0) {
+    const $el = $parent.childNodes[index];
+    if (!oldNode) {
+        //1. there's no newNode in the oldNode -> appendChild
+        $parent.appendChild(createElement(newNode));
+    } else if (!newNode) {
+        //2. there's oldNode but not newNode -> removeChild
+        $parent.removeChild($el);
+    } else if (changed(newNode, oldNode)) {
+        //3. node has changed -> replaceChild
+        $parent.replaceChild(
+            createElement(newNode), 
+            $el
+        );
+    } else if (newNode.type) {
+        const children = newNode.children.length < oldNode.children.length ? newNode.children : oldNode.children;
+        children.forEach((child, i) => {
+            updateElement($el, newNode.children[i], oldNode.children[i], i);
+        });
+    }
+}
+
+function changed(node1, node2) {
+    //TODO: check for changed props
+    return typeof node1 !== typeof node2 ||
+        (typeof node1 === 'string' && node1 !== node2) ||
+        node1.type !== node2.type
+        ;
+}
 
 const html = (
     <div>
